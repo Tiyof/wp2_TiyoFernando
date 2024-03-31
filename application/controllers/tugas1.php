308acc9852ed0e2ece38a84263568db2c1fe8a9e
Tiyof <tiyofernando26@gmail.com>
2024-03-31 15:32:46 +0700

    commit tugas1

---
+++ application/controllers/tugas1.php
@@ -0,0 +1,12 @@
+<?php
+class tugas1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Hallo Nama saya TIYO FERNANDO NAKLUI 
+            saya tiggal di jalan Apel Gang pisang raja No.31 olah raga yang saya sukai adalah badminton";
+            
+
+    }
+}
