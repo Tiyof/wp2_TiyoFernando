bfa3c0d73973763802cdf67522475df18c593c1c
Tiyof <tiyofernando26@gmail.com>
2024-04-22 16:13:59 +0700

    pertemuan 4 tugas 3

---
+++ application/views/v_header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Prog II Merancang Template Sederhana dengan codeinteger</title>
+    <link rel="stykesheet" type="text/css" href="<?php echo base_url()?>assets/css/stylebuku.css">
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>RENTAL BUKUku</h1>
+                <h3>Membuar Template sederhana dengan codeinteger</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a>href="<?php echo base_url() . 'Web' ?>Home"</a></li>
+                    <li><a>href="<?php echo base_url() . 'Web/about' ?>">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+            
+        </header>
+    </div>
+</body>
+</html>
